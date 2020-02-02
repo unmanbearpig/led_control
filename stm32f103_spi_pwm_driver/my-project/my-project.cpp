e6893138aa6b6e736ac5591b92ab4f26cf48be53
unmanbearpig <unmanbearpig@gmail.com>
2020-02-02 13:06:05 +0300

    Returns zeros, can't make it return something else

--- stm32f103_spi_pwm_driver/my-project/my-project.cpp
+++ stm32f103_spi_pwm_driver/my-project/my-project.cpp
@@ -42,19 +42,6 @@ LedValuesMessage msg =
    .led4_value = 0,
   };
 
-struct LedFadeValue {
-  tim_oc_id channel; // e.g. TIM_OC2
-  uint16_t value;
-  uint16_t min_value;
-  uint16_t max_value;
-  int16_t step;
-};
-
-struct LedValue {
-  tim_oc_id channel; // e.g. TIM_OC2
-  uint16_t value;
-};
-
 // SPI1
 // clock: RCC_SPI1
 // GPIOs:
@@ -80,9 +67,15 @@ void spi_setup() {
   // // ???
   // gpio_set_af(GPIOA, GPIO_AF5, GPIO5 | GPIO6 | GPIO7);
 
+  // SPI1_I2SCFGR = 0;
+
   spi_disable(SPI1);
   spi_reset(SPI1);
   spi_disable_ss_output(SPI1);
+  // spi_set_dff_8bit(SPI1); // seems like it's not needed
+
+  // need this?
+  spi_set_full_duplex_mode(SPI1);
 
   spi_set_slave_mode(SPI1);
   // spi_set_receive_only_mode(SPI1);
@@ -99,9 +92,19 @@ void spi_setup() {
   spi_set_clock_phase_0(SPI1);
   spi_send_msb_first(SPI1);
   spi_enable(SPI1);
-  spi_enable_rx_buffer_not_empty_interrupt(SPI1);
-  spi_enable_error_interrupt(SPI1);
+  // spi_enable_rx_buffer_not_empty_interrupt(SPI1);
+  // spi_enable_error_interrupt(SPI1);
 }
+
+void spi_dma_setup_output() {
+  dma_disable_peripheral_increment_mode(DMA1, DMA_CHANNEL3);
+  dma_channel_reset(DMA1, DMA_CHANNEL3);
+  dma_set_peripheral_address(DMA1, DMA_CHANNEL3, (uint32_t)&SPI1_DR);
+
+  nvic_set_priority(NVIC_DMA1_CHANNEL3_IRQ, 0);
+  nvic_enable_irq(NVIC_DMA1_CHANNEL3_IRQ);
+}
+
 
 // https://www.rhye.org/post/stm32-with-opencm3-2-spi-and-dma/
 void spi_dma_setup() {
@@ -113,7 +116,31 @@ void spi_dma_setup() {
 
  	nvic_set_priority(NVIC_DMA1_CHANNEL2_IRQ, 0);
 	nvic_enable_irq(NVIC_DMA1_CHANNEL2_IRQ);
+
+  spi_dma_setup_output();
 }
+
+void write_to_spi_dma(void *tx_buf, uint32_t tx_len) {
+  uint32_t dma = DMA1;
+  uint8_t channel = DMA_CHANNEL3;
+
+  dma_set_memory_address(dma, channel, (uint32_t)tx_buf);
+  dma_set_memory_size(dma, channel, DMA_CCR_MSIZE_8BIT);
+  dma_set_peripheral_size(dma, channel, DMA_CCR_PSIZE_8BIT);
+  dma_set_number_of_data(dma, channel, tx_len);
+  dma_set_priority(dma, channel, DMA_CCR_PL_HIGH);
+  dma_set_read_from_memory(dma, channel);
+  dma_enable_memory_increment_mode(dma, channel);
+  dma_enable_channel(dma, channel);
+
+  // not sure about this one, probably no
+  // dma_enable_circular_mode(dma, channel);
+
+  spi_enable_tx_dma(SPI1);
+
+  dma_enable_transfer_complete_interrupt(dma, channel);
+}
+
 
 void read_from_spi_dma(void *rx_buf, uint32_t rx_len) {
   uint32_t dma = DMA1;
@@ -138,14 +165,19 @@ void read_from_spi_dma(void *rx_buf, uint32_t rx_len) {
   // dma_enable_transfer_error_interrupt(dma, channel);
 }
 
-struct LedFadeValue led_fade_values[4] =
-  { { TIM_OC1, 0, 0x0000, 0x00FF, 10 },
-    { TIM_OC2, 0, 0x0000, 0x00FF, 13 },
-    { TIM_OC3, 0, 0x0000, 0x00FF,  5 },
-    { TIM_OC4, 0, 0x0000, 0x00FF,  7 } };
+LedValuesMessage tx_msg =
+  {
+   .magic = led_values_message_magic,
+   .led1_value = 0xF0F0,
+   .led2_value = 0x3322,
+   .led3_value = 0x0000,
+   .led4_value = 0xFFFF,
+  };
+
 
 void start_dma() {
   read_from_spi_dma(&input_msg, sizeof(input_msg));
+  write_to_spi_dma(&tx_msg, (sizeof(tx_msg)));
 }
 
 void stop_dma() {
@@ -178,34 +210,103 @@ static void set_msg_values_task(void *_value) {
   }
 }
 
-void dma1_channel2_isr() {
-  if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL2, DMA_TCIF)) {
-    if (is_msg_valid(&input_msg)) {
-      memcpy(&msg, &input_msg, sizeof(input_msg));
-    } else {
-      // We (the slave) might go out of sync with the host,
-      // i.e. we incorrectly assume the start of the message
-      // it might happen if the slave was (re)started in the middle of communication
-      // Here we try to resynchronize with the host by just restarting SPI DMA
-      // Eventually we should catch the correct start of the message and stop receiving errors
-      // There probably is a better way, but it works for now
-      restart_dma();
-      // Debugging, so I notice the errors better.
-      // Should change it no not changing state
-      set_msg_to_error_state(&msg);
-    }
+typedef struct {
+  uint32_t isrs;
+  uint32_t tcif_count;
+  uint32_t htif_count;
+  uint32_t teif_count;
 
+} DmaChanStats;
+
+DmaChanStats chan2_stats = { 0 };
+DmaChanStats chan3_stats = { 0 };
+
+void dma1_channel2_isr() {
+  chan2_stats.isrs += 1;
+
+  memcpy(&msg, &input_msg, sizeof(input_msg));
+
+  if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL2, DMA_TCIF)) {
+    chan2_stats.tcif_count += 1;
     dma_clear_interrupt_flags(DMA1, DMA_CHANNEL2, DMA_TCIF);
-  } else {
-    restart_dma();
   }
+
+  if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL2, DMA_HTIF)) {
+    chan2_stats.htif_count += 1;
+    dma_clear_interrupt_flags(DMA1, DMA_CHANNEL2, DMA_HTIF);
+  }
+
+  if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL2, DMA_TEIF)) {
+    chan2_stats.teif_count += 1;
+    dma_clear_interrupt_flags(DMA1, DMA_CHANNEL2, DMA_TEIF);
+  }
+
+
+  // if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL2, DMA_TCIF)) {
+  //   if (is_msg_valid(&input_msg)) {
+  //     memcpy(&msg, &input_msg, sizeof(input_msg));
+  //   } else {
+  //     // We (the slave) might go out of sync with the host,
+  //     // i.e. we incorrectly assume the start of the message
+  //     // it might happen if the slave was (re)started in the middle of communication
+  //     // Here we try to resynchronize with the host by just restarting SPI DMA
+  //     // Eventually we should catch the correct start of the message and stop receiving errors
+  //     // There probably is a better way, but it works for now
+  //     restart_dma();
+  //     // Debugging, so I notice the errors better.
+  //     // Should change it no not changing state
+  //     set_msg_to_error_state(&msg);
+  //   }
+
+  //   dma_clear_interrupt_flags(DMA1, DMA_CHANNEL2, DMA_TCIF);
+  // } else {
+  //   set_valid_msg_magic(&msg);
+  //   msg.led1_value = 0;
+  //   msg.led2_value = 0;
+  //   msg.led3_value = 0;
+  //   msg.led4_value = 0xFFFF;
+
+  //   restart_dma();
+  // }
 }
+
+void dma1_channel3_isr() {
+  chan3_stats.isrs += 1;
+
+  if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL3, DMA_TCIF)) {
+    chan3_stats.tcif_count += 1;
+    dma_clear_interrupt_flags(DMA1, DMA_CHANNEL3, DMA_TCIF);
+  }
+
+  if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL3, DMA_HTIF)) {
+    chan3_stats.htif_count += 1;
+    dma_clear_interrupt_flags(DMA1, DMA_CHANNEL3, DMA_HTIF);
+  }
+
+  if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL3, DMA_TEIF)) {
+    chan3_stats.teif_count += 1;
+    dma_clear_interrupt_flags(DMA1, DMA_CHANNEL3, DMA_TEIF);
+  }
+
+  spi_disable_tx_dma(SPI1);
+  write_to_spi_dma(&tx_msg, (sizeof(tx_msg)));
+
+  // breaks the leds when I comment it out, but the isrs number goes up
+  // dma_clear_interrupt_flags(DMA1, DMA_CHANNEL3, DMA_TCIF);
+}
+
+// static void write_to_spi_dma_task(void *_arg __attribute((unused))) {
+//   for(;;) {
+//     write_to_spi_dma(&tx_msg, (sizeof(tx_msg)));
+//     vTaskDelay(pdMS_TO_TICKS(10));
+//   }
+// }
 
 extern "C" int main(void) {
 	rcc_clock_setup_in_hse_8mhz_out_72mhz(); // For "blue pill"
 	rcc_periph_clock_enable(RCC_GPIOC);
 
-  // for spi pins
+  // for spi pins, seems to be not needed?
   // rcc_periph_clock_enable(RCC_GPIOA);
 
   // built-in LED
@@ -220,6 +321,7 @@ extern "C" int main(void) {
   pwm_setup(TIM_OCM_PWM2);
 
   xTaskCreate(set_msg_values_task, "SET_MSG_LED_VALUE", 100, (void *)&msg, configMAX_PRIORITIES-1, NULL);
+  // xTaskCreate(write_to_spi_dma_task, "START_DMA_TX_VALUE", 100, (void *)&msg, configMAX_PRIORITIES-1, NULL);
 
   start_dma();
 
