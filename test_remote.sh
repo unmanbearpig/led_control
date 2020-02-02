2a49b0ab08a1e422612cc89586aed634ab49373b
unmanbearpig <unmanbearpig@gmail.com>
2020-02-02 13:07:05 +0300

    add cleanup to testing script

--- test_remote.sh
+++ test_remote.sh
@@ -4,8 +4,12 @@ set -e
 
 HOST=$1
 
+make clean
+make
+
 rsync -av --delete ~/projects/stuff/electronics/led_control/ pi@$HOST:led_control/
 ssh pi@$HOST "pkill spi_fade_test" || true
+ssh pi@$HOST "make clean -C ~/led_control"
 ssh pi@$HOST "cd ~/led_control && ./test.sh"
 
 echo '---- done ----'
