b5bb1ff0e7818f3ea377a8c83e24a8d5eb893c87
unmanbearpig <unmanbearpig@gmail.com>
2020-02-13 18:20:19 +0300

    Extract linux gamepad header, print relative axis values

---
+++ common/linux_gamepad.h
@@ -0,0 +1,33 @@
+#include <inttypes.h>
+
+#define SHOULDERS 0x0F
+#define SHOULDER_LEFT_UP 0x01
+#define SHOULDER_LEFT_DOWN 0x04
+#define SHOULDER_RIGHT_UP 0x02
+#define SHOULDER_RIGHT_DOWN 0x08
+
+#define RIGHT_THUMB 0xF0
+#define RIGHT_THUMB_LEFT 0x80
+#define RIGHT_THUMB_RIGHT 0x20
+#define RIGHT_THUMB_UP 0x10
+#define RIGHT_THUMB_DOWN 0x40
+
+#define LEFT_JOYSTICK_BUTTON 0x40
+#define RIGHT_JOYSTICK_BUTTON 0x80
+#define SELECT_BUTTON 0x10
+#define START_BUTTON 0x20
+
+typedef struct {
+  uint8_t left_x;
+  uint8_t left_y;
+  uint8_t reserved_1;
+  uint8_t right_x;
+  uint8_t right_y;
+  uint8_t thumbs; // weird, = 0x0F when nothing is pressed down
+  uint8_t select_start_joystick_buttons_and_shoulders;
+  uint8_t reserved_2;
+} GamepadState;
+
+int8_t gamepad_abs_to_rel_axis(uint8_t abs) {
+  return abs - 0x80;
+}
--- linux_gamepad/main.c
+++ linux_gamepad/main.c
@@ -6,35 +6,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
-
-#define SHOULDERS 0x0F
-#define SHOULDER_LEFT_UP 0x01
-#define SHOULDER_LEFT_DOWN 0x04
-#define SHOULDER_RIGHT_UP 0x02
-#define SHOULDER_RIGHT_DOWN 0x08
-
-#define RIGHT_THUMB 0xF0
-#define RIGHT_THUMB_LEFT 0x80
-#define RIGHT_THUMB_RIGHT 0x20
-#define RIGHT_THUMB_UP 0x10
-#define RIGHT_THUMB_DOWN 0x40
-
-#define LEFT_JOYSTICK_BUTTON 0x40
-#define RIGHT_JOYSTICK_BUTTON 0x80
-#define SELECT_BUTTON 0x10
-#define START_BUTTON 0x20
-
-
-typedef struct {
-  uint8_t left_x;
-  uint8_t left_y;
-  uint8_t reserved_1;
-  uint8_t right_x;
-  uint8_t right_y;
-  uint8_t thumbs; // weird, = 0x0F when nothing is pressed down
-  uint8_t select_start_joystick_buttons_and_shoulders;
-  uint8_t reserved_2;
-} GamepadState;
+#include "../common/linux_gamepad.h"
 
 void report_error(int err) {
   char *error_str = strerror(err);
@@ -85,9 +57,9 @@ void print_gamepad(GamepadState *gamepad) {
   }
 
   printf(
-         "%02Xx%02X %02Xx%02X %c%c%c%c %c%c%c%c\n",
-         gamepad->left_x, gamepad->left_y,
-         gamepad->right_x, gamepad->right_y,
+         "%04dx%04d %04dx%04d %c%c%c%c %c%c%c%c\n",
+         gamepad_abs_to_rel_axis(gamepad->left_x), gamepad_abs_to_rel_axis(gamepad->left_y),
+         gamepad_abs_to_rel_axis(gamepad->right_x), gamepad_abs_to_rel_axis(gamepad->right_y),
          shoulder_left_up, shoulder_left_down,
          shoulder_right_up, shoulder_right_down,
          right_thumb_left, right_thumb_down,
@@ -96,8 +68,15 @@ void print_gamepad(GamepadState *gamepad) {
   fflush(stdout);
 }
 
+#define DEFAULT_GAMEPAD_PATH "/dev/hidraw0"
+
 int main(int argc __attribute((unused)), char *argv[] __attribute((unused))) {
-  int gamepad_fd = open("/dev/hidraw0", O_RDONLY);
+  char *gamepad_path = DEFAULT_GAMEPAD_PATH;
+  if (argc > 1) {
+    gamepad_path = argv[1];
+  }
+
+  int gamepad_fd = open(gamepad_path, O_RDONLY);
 
   if (gamepad_fd == -1) {
     report_error(errno);
