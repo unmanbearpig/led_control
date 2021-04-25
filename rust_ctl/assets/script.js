4155be23cfacf73a0f7213db471fa277e5f48a4c
unmanbearpig <unmanbearpig@gmail.com>
2021-04-25 11:30:36 +0300

    Remove console.log from javascript

--- rust_ctl/assets/script.js
+++ rust_ctl/assets/script.js
@@ -1,13 +1,10 @@
 // TODO fix mime type when serving it
-// TODO remove console.logs
 
 function sliderchange(event) {
-  console.log("sliderchange: ", event);
   var el = event.srcElement;
   var form = el.closest('form');
 
   var form_data = new FormData(form);
-  console.log("form_data: ", form_data);
 
   var req_vals = [];
   var entries = form_data.entries();
@@ -25,11 +22,8 @@ function sliderchange(event) {
 window.onload = function onload() {
   var sliders = document.getElementsByClassName("js-chan-slider");
 
-  console.log(sliders);
-
   for (let i = 0; i < sliders.length; i++) {
     var slider = sliders[i];
-    console.log("slider: ", slider);
 
     slider.oninput = sliderchange;
   };
