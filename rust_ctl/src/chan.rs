d5faa766e9b63186281c35160ffa2320feb56614
unmanbearpig <unmanbearpig@gmail.com>
2021-04-26 14:41:01 +0300

    Optional value adjustment

--- rust_ctl/src/chan.rs
+++ rust_ctl/src/chan.rs
@@ -4,9 +4,9 @@ use serde_derive::{Deserialize, Serialize};
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ChanConfig {
     pub index: u16,
-    pub min: f64,
+    pub min: f64, // min and max are ignored when exp is None
     pub max: f64,
-    pub exp: f64,
+    pub exp: Option<f64>,
     pub tags: Vec<String>,
     pub cuboid: Option<Cuboid>,
 }
@@ -18,7 +18,7 @@ impl Default for ChanConfig {
             index: 0,
             min: 0.0,
             max: 1.0,
-            exp: 2.2,
+            exp: None,
             tags: Vec::new(),
             cuboid: None,
         }
@@ -26,21 +26,29 @@ impl Default for ChanConfig {
 }
 
 impl ChanConfig {
-    pub fn adjust_value(&self, val: f32) -> f32 {
-        (
-            self.min +
-                (val as f64).powf(self.exp)
-                * (self.dynamic_range())
-        ) as f32
-    }
-
     pub fn dynamic_range(&self) -> f64 {
         self.max - self.min
     }
 
+    pub fn adjust_value(&self, val: f32) -> f32 {
+        if let Some(exp) = self.exp {
+            (
+                self.min +
+                    (val as f64).powf(exp)
+                    * (self.dynamic_range())
+            ) as f32
+        } else {
+            val
+        }
+    }
+
     pub fn unadjust_value(&self, val: f32) -> f32 {
-        (((val as f64) - self.min) / (self.dynamic_range()))
-            .powf(1.0 / self.exp) as f32
+        if let Some(exp) = self.exp {
+            (((val as f64) - self.min) / (self.dynamic_range()))
+                .powf(1.0 / exp) as f32
+        } else {
+            val
+        }
     }
 }
 
@@ -52,7 +60,7 @@ mod tests {
     #[test]
     fn test_adjust_chan_val_reverse() {
         let chan_config = ChanConfig {
-            index: 0, min: 0.15, max: 0.86, exp: 2.2,
+            index: 0, min: 0.15, max: 0.86, exp: Some(2.2),
             tags: Vec::new(), cuboid: None,
         };
 
